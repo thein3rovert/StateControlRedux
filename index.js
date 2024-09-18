9c563e52cc439ef92f3305865a1fd9adde07af63
thein3rovert <danielolaibi@gmail.com>
2024-09-18 16:14:21 +0100

    Added a new state

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 
-//  Create the type of the action - ENUM 
+// 1. Create the type of the ACTION - ENUM 
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CS_STUDENT_REMOVED = 'STUDENT_REMOVED'
 // Implement  the action creator
 // Define our action - object with a type property
 function ordercake() {
    return {
         type: CAKE_ORDERED,
         quantity: 1,
+    }
+}
+function csStudentRemoved() {
+    return {
+        type: CS_STUDENT_REMOVED,
+        description: "Number of computer science student removed from course",
     }
 }
 
@@ -20,19 +27,35 @@ function ordercake() {
 const initialState = {
     numOfCakes: 10,
 }
+// Create reducer for students
+const initialStudentState = {
+    enrolledStudent: 10,
+}
+
 //(PreviousState, action) => newState
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case CAKE_ORDERED:
             return {
                 //Create copy of state object
-               
+
                 numOfCakes: state.numOfCakes - 1,
             }
             default:
                 return state
     }
 } 
+
+const studentReducer = (state = initialStudentState, action) => {
+    switch(action.type){
+        case CS_STUDENT_REMOVED:
+            return {
+                enrolledStudent: state.enrolledStudent - 2,
+            }
+            default:
+                return state
+    }
+}
 
 // 1. Redux store holds the application state
 const store = createStore(reducer)
@@ -43,9 +66,20 @@ const unsubscribe = store.subscribe(() => console.log('update state', store.getS
 // 4. Povides a dispatch method to update the state
 store.dispatch(ordercake())
 store.dispatch(ordercake())
-unsubscribe()
 store.dispatch(ordercake())
 store.dispatch(ordercake())
 //5. Unsubscribe from the store by calling the Unsubscribe method in other to do 
 //that we have to add a return to the subscribe method above
 unsubscribe()
+
+const studentStore = createStore(studentReducer)
+
+console.log('Intial Student State', studentStore.getState())
+
+const unsubscribeStudent = studentStore.subscribe(() => console.log('update state', studentStore.getState()))
+// 4. Povides a dispatch method to update the state
+studentStore.dispatch(csStudentRemoved())
+studentStore.dispatch(csStudentRemoved())
+studentStore.dispatch(csStudentRemoved())
+unsubscribeStudent()
+
