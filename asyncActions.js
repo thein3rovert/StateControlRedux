f2fde97bc6e2f8018a34d325ebdfa80656858bcd
thein3rovert <danielolaibi@gmail.com>
2024-09-26 11:48:15 +0100

    AsyncActions Application
    Async Actions with Axios and Redux-thunk

--- asyncActions.js
+++ asyncActions.js
@@ -1,6 +1,11 @@
 const redux = require('redux')
 const createStore = redux.createStore
 const {reducer} = require('./reducers/asyncActionsReducer')
+const applyMiddleware = redux.applyMiddleware
+const {thunk} = require('redux-thunk')
+const axios = require('axios')
+const {fetchUsersSuccess, fetchUsersRequest, fetchUsersFailure} = require('./actions/asyncActionCreators')
+
 
 const initialState = {
     loading: false,
@@ -8,6 +13,28 @@ const initialState = {
     error: '',
 }
 
-const store = createStore(reducer)
-// Make API call and dispach the appropraite action we have in our as
-module.exports = {initialState}
+/* 
+======================
+Thunk Middleware action Creator.
+======================
+*/
+const fetchUsers = () => {
+    return function(dispatch) {
+        // Before we dispatch our api we have to dispatch our actions
+        dispatch(fetchUsersRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+            //respose.data is the users 
+            const users = response.data.map(user => user.id)
+            dispatch(fetchUsersSuccess(users))
+        }).catch(error => {
+           // error.message is the error message
+           dispatch(fetchUsersFailure(error.message))
+        })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+store.subscribe(() => {console.log(store.getState())})
+store.dispatch(fetchUsers())
+module.exports = {initialState};
